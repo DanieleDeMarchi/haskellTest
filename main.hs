c71a00647449459e3b5df7ed6c1f4be6a13209e2
DanieleDeMarchi <demarchidaniele9@gmail.com>
2020-07-16 16:25:07 +0000

    type, data

--- main.hs
+++ main.hs
@@ -4,5 +4,14 @@ fizz n | n `mod` 15 == 0  = "FizzBuzz"
        | n `mod` 5  == 0  = "Buzz"
        | otherwise = show n
 
-main :: IO()
-main = mapM_ putStrLn $ map fizz [1..100]
+main :: IO ()    -- This says that main is an IO action.
+main = return () -- This tells main to do nothing. (Serve a non dare errori in compilazione su repl)
+
+type Person = (Name,Address) 
+type Name = String
+data Address = None | Addr String deriving (Eq,Show)
+
+indirizzo :: Person -> String
+indirizzo p | snd p == None = "Nessun indirizzo"
+            | otherwise = show (snd p)
+
